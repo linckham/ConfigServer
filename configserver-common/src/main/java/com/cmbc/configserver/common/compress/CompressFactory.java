23aca5195bea90039b34cae253eb55c2d4dca0e8
linckham <linckham@gmail.com>
2014-11-08 09:24:07 +0800

    1.modify the CompressFactory's impletation logic.

--- configserver-common/src/main/java/com/cmbc/configserver/common/compress/CompressFactory.java
+++ configserver-common/src/main/java/com/cmbc/configserver/common/compress/CompressFactory.java
@@ -4,6 +4,7 @@ import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 /**
+ * the compress factory which can provide many different compress algorithm.
  * Created by tongchuan.lin<linckham@gmail.com><br/>
  *
  * @Date 2014/11/6
@@ -11,20 +12,27 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 public class CompressFactory {
     private static Map</**/Integer,Compress> compressMap = new ConcurrentHashMap<Integer,Compress>(8);
+
     public static Compress createCompress(CompressType type){
         Compress compress = compressMap.get(type.getType());
         if(null == compress){
-            switch (type.getType()){
-                case 0:
-                case 2:
-                case 1:
-                    compress = new QuickLZ();
-                    break;
-                default:
-                    compress = new QuickLZ();
-                    break;
+            //double check and lock the map
+            synchronized (compressMap){
+                compress = compressMap.get(type.getType());
+                if(null == compress){
+                    switch (type.getType()){
+                        case 0:
+                        case 2:
+                        case 1:
+                            compress = new QuickLZ();
+                            break;
+                        default:
+                            compress = new QuickLZ();
+                            break;
+                    }
+                    compressMap.put(type.getType(),compress);
+                }
             }
-            compressMap.put(type.getType(),compress);
         }
         return compress;
     }
