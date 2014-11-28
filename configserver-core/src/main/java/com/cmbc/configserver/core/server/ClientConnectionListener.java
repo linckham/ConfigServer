ee94a6958911788783b6f76025b5cbb43fa25398
tom.jiang1987@gmail.com <tom.jiang1987@gmail.com>
2014-11-28 10:10:50 +0800

    fix connection listener

--- configserver-core/src/main/java/com/cmbc/configserver/core/server/ClientConnectionListener.java
+++ configserver-core/src/main/java/com/cmbc/configserver/core/server/ClientConnectionListener.java
@@ -7,38 +7,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service("clientConnectionListener")
-public class ClientConnectionListener implements ChannelEventListener{
-    @Autowired
-    private HeartbeatService heartbeatService;
+public class ClientConnectionListener implements ChannelEventListener {
+	@Autowired
+	private HeartbeatService heartbeatService;
 
-    @Override
-    public void onChannelConnect(final NettyEvent event) {
-    }
+	@Override
+	public void onChannelConnect(final NettyEvent event) {
+	}
 
-    @Override
+	@Override
 	public void onChannelActive(final NettyEvent event) {
-		//add channel info
-		heartbeatService.channelCreated(event.getChannel());
+		// add channel info
+		if (null != event.getChannel() && event.getChannel().isActive()) {
+			heartbeatService.channelCreated(event.getChannel());
+		}
 	}
 
 	@Override
 	public void onChannelClose(final NettyEvent event) {
-        if (null != event.getChannel() && event.getChannel().isActive()) {
-            heartbeatService.clearChannel(event.getChannel());
-        }
-    }
+		heartbeatService.clearChannel(event.getChannel());
+	}
 
 	@Override
 	public void onChannelException(final NettyEvent event) {
-        if (null != event.getChannel() && event.getChannel().isActive()) {
-            heartbeatService.clearChannel(event.getChannel());
-        }
+		heartbeatService.clearChannel(event.getChannel());
 	}
 
 	@Override
 	public void onChannelIdle(final NettyEvent event) {
-        if (null != event.getChannel() && event.getChannel().isActive()) {
-            heartbeatService.clearChannel(event.getChannel());
-        }
-    }
+		heartbeatService.clearChannel(event.getChannel());
+	}
 }
